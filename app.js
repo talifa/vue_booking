a43e658e004c116324d1561448fb8ccdf528d607
talifa <talifaidk@gmail.com>
2019-08-12 14:51:13 +0300

    auth

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var express = require('express'),
     app = express(),
     path = require('path'),
     session = require('express-session');
+
+app.use(express.urlencoded({ extended: false }))
 app.use(session({
     secret: '2C44-4D44-WppQ38S',
     resave: true,
@@ -24,18 +26,19 @@ app.use(session({
 
 // Authentication and Authorization Middleware
 var auth = function (req, res, next) {
-    if (req.session && req.session.user === "amy" && req.session.admin)
+    if (req.session && req.session.user === "123" && req.session.admin)
         return next();
     else
+
         return res.sendStatus(401);
 };
-
-// Login endpoint
-app.get('/login', function (req, res) {
+// localhost:3000/auth?username=123&password=123
+// auth endpoint
+app.get('/auth', function (req, res) {
     if (!req.query.username || !req.query.password) {
         res.send('login failed');
-    } else if (req.query.username === "amy" || req.query.password === "amyspassword") {
-        req.session.user = "amy";
+    } else if (req.query.username === "123" && req.query.password === "123") {
+        req.session.user = "123";
         req.session.admin = true;
         res.send("login success!");
     }
